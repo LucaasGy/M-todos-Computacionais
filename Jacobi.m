6c6e3a8c2ec799919df0e219ddd5f70d150ee28f
Lucas Gabriel <lucasfacul20222@gmail.com>
2025-05-09 12:12:08 -0300

    fix: Corrigindo erros

--- Jacobi.m
+++ Jacobi.m
@@ -1,11 +1,11 @@
 function [x, k, Erx, msg] = jacobi(A, b, tol, N, x0)
     n = length(b);
     x_old = x0;
-    x = x0;
+    x = zeros(n, 1);
     C = zeros(n);
     d = zeros(n, 1);
-    
-    % Verifica se a matriz é diagonal dominante
+
+    % Verifica se A é diagonal dominante
     for i = 1:n
         soma = sum(abs(A(i, :))) - abs(A(i, i));
         if abs(A(i, i)) <= soma
@@ -15,7 +15,7 @@ function [x, k, Erx, msg] = jacobi(A, b, tol, N, x0)
         end
     end
 
-    % Construção das matrizes C e d
+    % Matriz C e vetor d
     for i = 1:n
         for j = 1:n
             if i ~= j
@@ -28,42 +28,42 @@ function [x, k, Erx, msg] = jacobi(A, b, tol, N, x0)
     Erx = inf;
     k = 0;
 
-    fprintf('Iter\t\t x1\t\t x2\t\t x3\t\t Erro(%%)\n');
-    fprintf('-----------------------------------------------------------\n');
+    fprintf('Iter\t\t');
+    for i = 1:n
+        fprintf('x%d\t\t', i);
+    end
+    fprintf('Erro(%%)\n');
+    fprintf('---------------------------------------------------\n');
 
     while k < N && Erx > tol
         x = C * x_old + d;
 
-        Er = abs((x - x_old) ./ x);
-        Er(isnan(Er)) = 0;  % evitar divisão por zero
-        Erx = max(Er) * 100;
+        % Erro percentual com norma 2
+        Erx = norm(x - x_old) / (norm(x + eps)) * 100;
 
-        % Exibe os resultados da iteração
-        fprintf('%d\t\t %.6f\t %.6f\t %.6f\t %.6f\n', k+1, x(1), x(2), x(3), Erx);
-
-        if Erx < tol
-            msg = 'Convergência atingida com sucesso!';
-            return;
-        end
+        fprintf('%d\t\t', k + 1);
+        fprintf('%.6f\t', x);
+        fprintf('%.6f\n', Erx);
 
         x_old = x;
         k = k + 1;
     end
 
-    if k == N
-        msg = 'Erro: número máximo de iterações atingido sem convergência.';
-    else
+    if Erx <= tol
         msg = 'Convergência atingida com sucesso!';
+    else
+        msg = 'Erro: número máximo de iterações atingido sem convergência.';
     end
 end
 
-A = [10, 3, -2;
-     2, 8, -1;
+
+A = [10, 2, -1;
+     -3, -6, 2;
      1, 1, 5];
 
-b = [57; 20; -4];
+b = [27; -61.5; -21.5];
 x0 = [0; 0; 0];
-tol = 0.0005;
+tol = 0.05;
 N = 100;
 
 [x, k, Erx, msg] = jacobi(A, b, tol, N, x0);
